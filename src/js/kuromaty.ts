62c645f2d68232436e1c5f6e1827147cd9fbf7cf
Kuro <kuromatch@gmail.com>
2017-11-14 05:46:53 +0900

    🚑 売買ボリュームの値が無い時に対応

--- src/js/kuromaty.ts
+++ src/js/kuromaty.ts
@@ -962,7 +962,7 @@ export class Kuromaty {
 
                 this.overlay.context.restore();
 
-                if (this.cursorX > -1) {
+                if (this.cursorX > -1 && chart._bars[0].length > BarColumn.BuyVolume) {
                     this._drawDepthIndicator(
                         this.overlay.context,
                         chartW - chartM - 7,
